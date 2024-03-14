2aaf8ef2a406c5ccc3cf6057331ff31ba263a8c4
Shadrack BODJE <shadrackemmanuel.bodje@ynov.com>
2024-03-14 12:38:47 +0100

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,20 +1,18 @@
-# Librairies
+'''
+load the data
+'''
+
 import csv
 import pandas as pd
 
-
-# Data
 URL = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-tertiaire-2/lines?size=10000&format=csv&after=10000%2C965634&header=true"
 
 data = pd.read_csv(URL)
 
-# Verifier que data contient des donnée
 assert len(data) > 0
 
-# print la taille de data
 print(data.shape)
 
-# Enregistre le fichier qu'on crée en sortie
+OUTPUT_FILE = "./data/dpe_tertiaire_20240314.csv"
 
-output_file = f"./data/dpe_tertiaire_20240314.csv"
-data.to_csv(output_file, index=False, quoting=csv.QUOTE_ALL)
+data.to_csv(OUTPUT_FILE, index=False, quoting=csv.QUOTE_ALL)
